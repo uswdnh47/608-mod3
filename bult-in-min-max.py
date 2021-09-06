8176a7b46029c9a8ac6d0d676d76178f3354a3e9
Deshawn Hunter <89186945+uswdnh47@users.noreply.github.com>
2021-09-05 23:55:57 -0500

    Update bult-in-min-max.py

--- bult-in-min-max.py
+++ bult-in-min-max.py
@@ -1,1 +1,13 @@
-
+def maximum (value1, value2, value3):
+    max_value = value1
+    if value2 > max_value:
+        max_value = value2
+    if value3 > max_value:
+        max_value = value3
+    return max_value
+  
+maximum (12,27,36)
+  
+max (12, 27, 36)
+  
+min(15, 9, 27, 14) 

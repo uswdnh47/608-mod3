f0a6d5d94cfd76a960cbeeb9a601daf38f9e2184
Deshawn Hunter <89186945+uswdnh47@users.noreply.github.com>
2021-09-06 11:08:48 -0500

    Update custom-object.py
    File 4 - custom-object.py (read Sections 4.8-4.17)
    1. In this file, we will create a custom object with corresponding methods (object functions) - see Module 3: Project Purchase Object Methods 
    2. Using the information provided, implement the following methods:
    1. calculateTax()
    2. calculateTip()
    Call the methods and verify you get the same values as the example provided.

--- custom-object.py
+++ custom-object.py
@@ -1,1 +1,24 @@
+class Purchase(object):
+    def __init__(self, amount):
+        self.amount = amount
 
+    def calculateTax(self, taxPercent):
+        return self.amount * taxPercent/100.0
+
+    def calculateTip(self, tipPercent):
+        return self.amount * tipPercent/100.0
+
+    def calculateTotal(self, taxPercent, tipPercent):
+        return self.amount * (1 + taxPercent/100.0 + tipPercent/100.0)
+
+purchase = Purchase(100.0)
+
+taxPercent = 7.5
+tipPercent = 20.0
+
+tax = purchase.calculateTax(taxPercent)
+tip = purchase.calculateTip(tipPercent)
+
+print ('Tax:', tax)
+print ('Tip:', tip)
+print ('Total:', purchase.calculateTotal(taxPercent, tipPercent))
